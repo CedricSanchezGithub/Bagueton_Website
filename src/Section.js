1d053103d3794add882feace214821e02ac7d3c4
cedric <mycedmail@gmail.com>
2024-03-09 20:36:23 +0100

    update Section.js and output.css

--- src/Section.js
+++ src/Section.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Section({ text, imagePath, altText, background, height }) {
     return (
         <div className={`section flex ${background} ${height}`}>
-            <div className='max-w-xl m-auto lg:grid lg:grid-cols-2 flex flex-col items-center justify-center text-xl'>
+            <div className='max-w-2xl  m-auto lg:grid lg:grid-cols-2 flex flex-col items-center justify-center text-xl'>
                 <div className="text-content p-4 text-orange-950 items-center mx-auto" data-aos="zoom-in-right">
                     {text}
                 </div>
